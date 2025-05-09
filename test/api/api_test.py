0e8f4bab4cc3ea10abec603e60529cc976d81706
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2025-05-09 14:54:49 -0300

    Testes de api dando muito problema

--- test/api/api_test.py
+++ test/api/api_test.py
@@ -3,14 +3,15 @@ import pytest
 from fastapi.testclient import TestClient
 from api.api import *
 
+from mocks import db_register_mock
+
 @pytest.fixture
 def cliente():
     return TestClient(app)
 
 def test_hello(client):
+    mock = db_register_mock()
+    
     response = client.post("/register/")
     assert response.status_code == 200
-    assert response.json() == {
-        "user_email": "guilherme@email.com",
-        "user_password": "123456",
-        "user_name": "Guilherme"}
+    assert response.json() == mock
